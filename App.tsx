7bb6f6f8ad62eed300d93184c16b4612a0b57dd7
felipe.ospina <felipe.ospina@globant.com>
2022-10-21 09:34:22 -0500

    add assets types

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View } from 'react-native';
+import group from './assets/group.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* <Image source={group} style={{width:300, height:200}}/> */}
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundImage: group,
   },
 });
